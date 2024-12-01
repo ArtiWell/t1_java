2ac71f81a08aef99f3c6dc0a061a341d6f0f34c2
artem.s <empty>
2024-12-02 01:40:44 +0300

    Задание 4 sec&test

---
+++ service2/src/main/java/ru/t1/java/service2/config/JwtAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package ru.t1.java.service2.config;
+
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String token = authorizationHeader.substring(7);
+            if (validateToken(token)) {
+
+                SecurityContextHolder.getContext().setAuthentication(getAuthentication(token));
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    private boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private Authentication getAuthentication(String token) {
+        return new UsernamePasswordAuthenticationToken("service1", null, List.of());
+    }
+}
--- src/main/java/ru/t1/java/demo/service/TransactionService.java
+++ src/main/java/ru/t1/java/demo/service/TransactionService.java
@@ -1,13 +1,17 @@
 package ru.t1.java.demo.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClient;
 import ru.t1.java.demo.emums.AccountStatus;
 import ru.t1.java.demo.emums.TransactionStatus;
 import ru.t1.java.demo.entity.AccountEntity;
@@ -19,6 +23,8 @@ import ru.t1.java.demo.model.dto.TransactionMessageDTO;
 import ru.t1.java.demo.dao.persistence.AccountRepository;
 import ru.t1.java.demo.dao.persistence.TransactionRepository;
 
+import java.util.Date;
+
 import static ru.t1.java.demo.mapper.TransactionMapper.TRANSACTION_MAPPER;
 
 @Service
@@ -29,13 +35,19 @@ public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final AccountRepository accountRepository;
     private final KafkaTemplate<String, TransactionMessageDTO> kafkaTemplate;
+    private final RestClient restClient;
+
+    @Value("${transaction.reject-threshold}")
+    private int rejectThreshold;
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
 
 
-    public Page<TransactionResponse> getAllTransaction(Pageable pageable) {
 
-        return transactionRepository.findAll(pageable)
-                .map(TRANSACTION_MAPPER::toResponse);
-    }
 
 
     @Transactional
@@ -47,16 +59,84 @@ public class TransactionService {
             throw new IllegalStateException("Account with ID " + transactionRequest.accountId() + " is not open for transactions.");
         }
 
-        account.setBalance(account.getBalance() + transactionRequest.amount());
+        boolean isClientBlocked = account.getClient().isBlocked();
+
+        if (!isClientBlocked) {
+            isClientBlocked = checkClientStatus(account.getClient().getId());
+            if (isClientBlocked) {
+                handleBlockedClient(account);
+                return saveRejectedTransaction(account, transactionRequest.amount());
+            }
+        }
+
+        long rejectedCount = transactionRepository.countByAccountAndStatus(account, TransactionStatus.REJECTED);
+
+        if (rejectedCount >= rejectThreshold) {
+            handleArrestedAccount(account);
+            return saveRejectedTransaction(account, transactionRequest.amount());
+        }
+
         TransactionEntity transaction = TRANSACTION_MAPPER.toEntity(transactionRequest, account);
-
-        transaction.setStatus(TransactionStatus.REQUESTED);
-        transactionRepository.save(transaction);
-
         sendTransactionAcceptMessage(account, transaction);
 
+        return processTransaction(account, transactionRequest, transaction);
+    }
+
+    private boolean checkClientStatus(Long clientId) {
+        String url = "/api/check-client";
+        log.info("Checking client status for client ID {}.", clientId);
+
+        return Boolean.TRUE.equals(restClient.get()
+                .uri(uriBuilder -> uriBuilder.path(url).queryParam("clientId", clientId).build())
+                .header("Authorization", "Bearer " + generateJwtToken())
+                .retrieve()
+                .body(Boolean.class));
+    }
+
+    private void handleBlockedClient(AccountEntity account) {
+        account.getClient().setBlocked(true);
+        account.setStatus(AccountStatus.BLOCKED);
+        accountRepository.save(account);
+        log.info("Client ID {} and account ID {} marked as BLOCKED.", account.getClient().getId(), account.getId());
+    }
+
+    private void handleArrestedAccount(AccountEntity account) {
+        account.setStatus(AccountStatus.ARRESTED);
+        accountRepository.save(account);
+        log.info("Account ID {} marked as ARRESTED.", account.getId());
+    }
+
+    private TransactionResponse saveRejectedTransaction(AccountEntity account, Long amount) {
+        TransactionEntity transaction = new TransactionEntity();
+        transaction.setAccount(account);
+        transaction.setAmount(amount);
+        transaction.setStatus(TransactionStatus.REJECTED);
+        transactionRepository.save(transaction);
+        log.info("Transaction for account ID {} rejected.", account.getId());
         return TRANSACTION_MAPPER.toResponse(transaction);
     }
+
+    private TransactionResponse processTransaction(AccountEntity account, TransactionRequest request, TransactionEntity transaction) {
+        transaction.setStatus(TransactionStatus.ACCEPTED);
+        account.setBalance(account.getBalance() + request.amount());
+        transactionRepository.save(transaction);
+        accountRepository.save(account);
+        log.info("Transaction for account ID {} accepted.", account.getId());
+        return TRANSACTION_MAPPER.toResponse(transaction);
+    }
+
+    private String generateJwtToken() {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpiration);
+
+        return Jwts.builder()
+                .setSubject("service1")
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
 
     private void sendTransactionAcceptMessage(AccountEntity account, TransactionEntity transaction) {
         TransactionMessageDTO message = new TransactionMessageDTO(
@@ -72,9 +152,22 @@ public class TransactionService {
         log.info("Transaction accept message sent to Kafka for transaction ID {}", transaction.getId());
     }
 
+
+
+
+
+
+
+
     public TransactionResponse getTransactionById(Long id) {
         return TRANSACTION_MAPPER.toResponse(transactionRepository.findById(id).orElseThrow(
                 ()-> new EntityNotFoundException("Transaction with ID "+id+" not found")
         ));
+    }
+
+    public Page<TransactionResponse> getAllTransaction(Pageable pageable) {
+
+        return transactionRepository.findAll(pageable)
+                .map(TRANSACTION_MAPPER::toResponse);
     }
 }
